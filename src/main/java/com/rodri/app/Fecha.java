79004b6a2ef760db83d3293e680d7abb33550028
Rodrigo Pascual Montero <rodrigopascual1@hotmail.com>
2020-10-17 19:29:31 +0200

    [ADD] Ejercicio Maven Fecha

---
+++ src/main/java/com/rodri/app/Fecha.java
@@ -0,0 +1,48 @@
+package com.rodri.app;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Fecha
+ */
+
+public class Fecha extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Fecha() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter pw = response.getWriter();
+		Date date = new Date();
+		
+		pw.print("<html><body>");
+		pw.print("<p>La hora es:" + date +  "</p>");
+		pw.print("</body></html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,7 @@
+<html>
+<body>
+<form action="localhost:8080/FechaMaven/Fecha" method="get">
+<h2>Para saber la fecha haz clic en este <a href="localhost:8080/FechaMaven/Fecha">enlace</a></h2>
+</form>
+</body>
+</html>
